9915871eff43c86c369d52ac27e5dd6358f13072
VertoDebru <t.vervoot@gmail.com>
2021-12-21 21:41:21 +0100

    Add checkForm method.

--- front/js/class/MyProduct.js
+++ front/js/class/MyProduct.js
@@ -1,7 +1,7 @@
 class Myproduct {
     constructor(data) {
         this.data = data;
-        this.cart = "";
+        this.cart = CheckCart();
     }
 
     // Mise en page du produit.
@@ -32,7 +32,88 @@ class Myproduct {
         quantity.value = 1;
         // Ajout de l'evenement sur le bouton.
         addButton.addEventListener("click", () => {
-            this.Check();
+            this.checkForm();
         });
     }
+
+    // Verification du formulaire.
+    checkForm() {
+        // Verifie si les champs formulaire sont bien rempli.
+        if(!colorsProduct.value) {
+            alert("Aucune couleur défini!");
+            colorsProduct.focus();
+        }
+        if(quantity.value <= 0) {
+            alert("Aucune quantité défini!");
+            quantity.focus();
+        }
+        // Verifie si le formulaire est bien rempli.
+        if(colorsProduct.value && quantity.value >= 1) {
+            // Si le panier est vide, on ajoute le produit.
+            if(IsEmptyCart()) {
+                this.addProduct();
+            // Si le panier contient des articles.
+            } else {
+                // Si le produit est deja present dans le panier.
+                if(CheckInCart(this.data._id)) {
+                    let articleId = this.getArticleId(this.data._id);
+                    // Si on recois un identifiant.
+                    if(articleId) {
+                        // Boucle sur tous les produits contenu dans le panier.
+                        for(let i in this.cart)
+                            if(i == articleId) this.editProduct(i);
+                    // Si on recois aucun identifiant.
+                    } else {
+                        this.addProduct();
+                    }
+                // Si le produit n'est pas dans le panier.
+                } else {
+                    this.addProduct();
+                }
+            }
+        }
+    }
+
+    // Ajout d'un produit dans le panier.
+    addProduct() {
+        // On ajoute un nouveau produit au panier.
+        this.cart.push([this.data._id,parseInt(quantity.value),colorsProduct.value]);
+        localStorage.setItem('basket', JSON.stringify(this.cart));
+        alert(`==> classProduct : ${quantity.value} ${this.data.name} en ${colorsProduct.value} ajouté au panier.`);
+        return;
+    }
+
+    // Modification de la quantité du produit.
+    editProduct(Id) {
+        // Mise a jour de la quantité. (Quantité actuelle + Quantité souhaité)
+        this.cart[Id][1] = parseInt(this.cart[Id][1])+parseInt(quantity.value);
+        localStorage.setItem('basket', JSON.stringify(this.cart));
+        alert(`==> classProduct : ${quantity.value} ${this.data.name} en ${colorsProduct.value} à été rajouté. (Actuellement : ${this.cart[Id][1]})`);
+        return;
+    }
+
+    // Recupere l'identifiant de l'article
+    // Selon l'Id et la couleur.
+    getArticleId(Id) {
+        for(let i in this.cart) {
+            if(this.cart[i][0] == Id && this.cart[i][2] == colorsProduct.value) return i;
+        }
+    }
+}
+
+// Verifie si le panier est vide. (Renvoi : true/false)
+function IsEmptyCart() {
+    let myCart = CheckCart();
+    if(!myCart.length)
+        return true;
+    if(myCart.length)
+        return false;
+}
+
+// Verifie si un produit existe dans le panier. (Renvoi: true/false)
+function CheckInCart(Id) {
+    for(let i in CheckCart()) {
+        if(CheckCart()[i][0] == Id) return true;
+    }
+    return false;
 }
